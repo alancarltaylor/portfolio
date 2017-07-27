57cec007b6b1612907f2d6d3375e2852896a9579
Alan Taylor <alancarltaylor@gmail.com>
2017-07-26 21:04:29 -0600

    totally doesnt work, but initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+// Define the routes
+const ROUTES = [
+  {
+    path: '',
+    component: HomeComponent
+  }
+  // ,
+  // {
+  //   path: 'sites',
+  //   component: SitesComponent,
+  //   children: [
+  //   { path: ':id', component: SiteComponent}
+  //   ]
+  // }
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
