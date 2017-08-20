a618063ee3f615ed88df2f99971cdd94f46c558a
Alan Taylor <alancarltaylor@gmail.com>
2017-08-20 17:46:01 -0600

    trying to add animation to side tabs, broken now

--- src/app/sillyness/sillyness.component.ts
+++ src/app/sillyness/sillyness.component.ts
@@ -1,20 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { SharedService } from '../shared.service';
 
+// import slide in/out animation
+import { slideInOutAnimation } from '../_animations/index';
+
 @Component({
   selector: 'app-sillyness',
   templateUrl: './sillyness.component.html',
-  styleUrls: ['./sillyness.component.scss']
+  styleUrls: ['./sillyness.component.scss'],
+  // make slide in/out animation available to this component
+  animations: [slideInOutAnimation],
+
+  // attach the slide in/out animation to the host (root) element of this component
+  host: { '[@slideInOutAnimation]': 'app-home' }
 })
 export class SillynessComponent implements OnInit {
   message: string[] = [];
   constructor(private sharedService: SharedService) { }
 
   ngOnInit() {
+    console.log("i love how consistently console logs work")
   }
 
-  onClick(){
-    window.open("https://vidiot.herokuapp.com");
+  onClick(e){
+    window.open(e.srcElement.parentNode.id);
   }
 
 }
