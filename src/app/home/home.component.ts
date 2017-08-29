82202ecc6525380fbecc7cbbdb78073b3559eac1
Alan Taylor <alancarltaylor@gmail.com>
2017-08-28 23:21:29 -0600

    its too late and brain not working

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit, ElementRef, ViewChild, Inject, AfterViewChecked, EventEmitter, HostListener } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, Renderer, Inject, AfterViewChecked, EventEmitter, HostListener } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { DOCUMENT } from '@angular/platform-browser';
 import { NgFor } from '@angular/common';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 
 
 import { SharedService } from '../shared.service';
@@ -14,54 +14,86 @@ import { SharedService } from '../shared.service';
 })
 export class HomeComponent implements OnInit, AfterViewChecked{
   @ViewChild('scrollMe') private myScrollContainer: ElementRef;
-  @HostListener('document:touchstart', ['$event'])
-  clickout(event) {
-    console.log("thing i'm clicking", this._eref);
-    console.log("event target: ", event);
-    // alert(this.clicked);
-    if ((event.path[0]) === "app-sillyness.ng-tns-c2-0.ng-trigger.ng-trigger-slideInOutAnimation"){
-      // alert("clicked outside the terminal thing, great!");
-      this.myFocusTriggeringEventEmitter.emit(false);
-    } else {
-      this.myFocusTriggeringEventEmitter.emit(true);
-    }
-    // doSomething();
 
-    this.clicked = !this.clicked;
-  }
 
 
   userInput: string;
   clicked: boolean = true;
+  tablet: boolean;
   history: any[] = [];
   pastCommands: string[] = [];
   commands: string[] = ["info", "projects", "resume", "linkedin", "bio", "github"];
   keyUpCount: number = 0;
   openProjects: any;
+  private visible: boolean = false;
+  private unregister: Function;
   @ViewChild('terminal') terminal:ElementRef;
 
   constructor(@Inject(DOCUMENT) private document: Document,
               private sharedService: SharedService,
               private router: Router,
               private _eref: ElementRef,
+              private renderer: Renderer,
+              private activatedRoute: ActivatedRoute
             ) {
               const mql: MediaQueryList = window.matchMedia('(min-width: 600px)');
               document.body.style.margin = "0px";
               mql.addListener((mql: MediaQueryList) => {
-                this.openProjects = mql.matches ?
-                this.router.navigate(['./projects']) :
-                window.open("https://www.linkedin.com/in/alan-taylor-84870386/");
+                mql.matches ?
+                this.tablet = false :
+                this.tablet = true;
           });
   }
 
 
   public myFocusTriggeringEventEmitter = new EventEmitter<boolean>();
 
-  // focusFunction(){
-  //   // this.clicked = !this.clicked;
-  //   console.log("different console log statement so i know things are saving");
-  //   this.myFocusTriggeringEventEmitter.emit(this.clicked);
+  @HostListener('document:click', ['$event'])
+  clickout(event) {
+console.log(this.activatedRoute.url.toString());
+    this.visible = !this.visible;
+        if (this.activatedRoute.url[0] === "projects") {
+            this.unregister = this.renderer.listenGlobal('document', 'touchstart', () => {
+                this.myFocusTriggeringEventEmitter.emit(false);
+                this.unregister();
+            });
+        } else {
+          this.myFocusTriggeringEventEmitter.emit(true);
+        }
+    // let thing = 1;
+    // console.log("thing i'm clicking", this._eref);
+    // console.log("event target: ", event);
+    // console.log("event.path: ", event.path[0].className);
+  //   if (event.path[0].className == "prompt output new-output"){
+  //
+  //   this.myFocusTriggeringEventEmitter.emit(true);
   // }
+    // if (this.clicked){
+    // }
+
+    this.clicked = !this.clicked;
+    // alert(this.clicked);
+    // if ((event.path[0].className) === "ng-tns-c2-0 ng-trigger ng-trigger-slideInOutAnimation"){
+    //   // alert("clicked outside the terminal thing, great!");
+    //   this.myFocusTriggeringEventEmitter.emit(false);
+    // } else {
+    //   this.myFocusTriggeringEventEmitter.emit(true);
+    // }
+    // doSomething();
+
+    // this.clicked = !this.clicked;
+  }
+
+  focusFunction(){
+    console.log("different console log statement so i know things are saving");
+    this.myFocusTriggeringEventEmitter.emit(true);
+  //   if (this.clicked && this.tablet){
+  //     this.myFocusTriggeringEventEmitter.emit(true)
+  // } else if (!this.tablet){
+  //   this.myFocusTriggeringEventEmitter.emit(true)
+  // }
+  //   this.clicked = true;
+  }
 
   ngOnInit() {
         this.scrollToBottom();
@@ -130,7 +162,8 @@ export class HomeComponent implements OnInit, AfterViewChecked{
         // " "];
         dto = {input: userInput, output: message};
         this.showResponse(dto);
-        this.openProjects;
+        // this.handleScreenSize();
+        this.router.navigate(['./projects']);
         break;
     case "close":
         message = [];
@@ -168,6 +201,13 @@ export class HomeComponent implements OnInit, AfterViewChecked{
       this.history.push(i);
     }
     this.userInput = "";
+  }
+
+  handleScreenSize(){
+    this.tablet ?
+    window.open("https://docs.google.com/document/d/1hOighgLyBw8wOcHxPK4hMqAfHg6z0Pksab1vy_3BhB8/edit?usp=sharing") :
+    this.router.navigate(['./projects']);
+
   }
 
   usePastCommands(e){
